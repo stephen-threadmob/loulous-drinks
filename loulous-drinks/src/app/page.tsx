fdcf84a60ca78e9dd8757de2374286dea477f96a
stephen-threadmob <stephensmith@threadmob.com>
2026-07-21 22:18:09 -0400

    Delete loulous-drinks directory

--- loulous-drinks/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from "next/navigation";
-import { publicEnv } from "@/lib/env";
-
-// The root of the site sends guests to the default restaurant's menu. In a
-// multi-restaurant future this could become a landing page.
-export default function Home() {
-  redirect(`/${publicEnv.defaultRestaurantSlug}`);
-}
